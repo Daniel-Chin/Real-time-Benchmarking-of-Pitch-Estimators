8fea6e1e3bbdde4561f0136d69f5b80b126016d4
Daniel Chin <daniel_chin@yahoo.com>
2020-12-21 21:46:27 +0800

    main code complete

--- field/pitchDetect.py
+++ field/pitchDetect.py
@@ -1,26 +1,34 @@
 import pyaudio
 from pyaudio import paFloat32
+from time import sleep
 
-CHUNK = 2048
+from yin import estimatePitch
+
+FRAME_LEN = 2048
 SR = 44100
 
 def main():
   pa = pyaudio.PyAudio()
   stream = pa.open(
     format = paFloat32, channels = 1, rate = SR, 
-    input = True, frames_per_buffer = CHUNK
+    input = True, frames_per_buffer = FRAME_LEN
   )
   try:
     while True:
       frame = getLatestFrame(stream)
-      estimatePitch(frame, CHUNK, SR)
+      estimatePitch(frame, FRAME_LEN, SR)
   finally:
     stream.stop_stream()
     stream.close()
     pa.terminate()
 
 def getLatestFrame(stream):
-  data = stream.read(CHUNK)
-  stream.get_read_available
+  waste = stream.get_read_available() - FRAME_LEN
+  while waste < 0:
+    sleep(.001)
+    waste = stream.get_read_available() - FRAME_LEN
+  stream.read(waste)
+  print('waste', waste)
+  return stream.read(FRAME_LEN)
 
 main()
